733e357c48e9999ed3b90e805b17da315d584c90
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2024-03-24 17:14:39 +0530

    Grid view count

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final String title = "Hello Horizontal";
+    const title = 'Grid List';
 
     return MaterialApp(
       title: title,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: const Text(title),
         ),
-        body: Container(
-          margin: const EdgeInsets.symmetric(vertical: 20),
-          height: 200,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Container(
-                width: 160,
-                color: Colors.red[800],
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(50, (index) {
+            return Center(
+              child: Text(
+                'Number $index',
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
-              Container(
-                width: 160,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 160,
-                color: Colors.green,
-              ),
-              Container(
-                width: 160,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 160,
-                color: Colors.orange,
-              ),
-            ],
-          ),
+            );
+          }),
         ),
       ),
     );
