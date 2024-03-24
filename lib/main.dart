79965ff63d9cf7daaa092d201339cb4f2221bd90
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2024-03-24 12:46:17 +0530

    prj gallery

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,21 @@ class MyApp extends StatelessWidget {
         ),
         body: const SingleChildScrollView(
           child: Column(
-            children: [TitleSection(name: "hello", location: "hello")],
+            children: [
+              ImageSection(image: 'images/lake.jpg'),
+              TitleSection(name: "hello", location: "hello"),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
+            ],
           ),
         ),
       ),
@@ -46,11 +60,147 @@ class TitleSection extends StatelessWidget {
                   name,
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
+              ),
+              Text(
+                location,
+                style: TextStyle(color: Colors.grey[500]),
               )
             ],
-          ))
+          )),
+          const FavWget()
         ],
       ),
     );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonCreate(color: color, icon: Icons.call, lable: "Call"),
+          ButtonCreate(color: color, icon: Icons.near_me, lable: "Route"),
+          ButtonCreate(color: color, icon: Icons.share, lable: "Share")
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonCreate extends StatelessWidget {
+  const ButtonCreate(
+      {super.key,
+      required this.color,
+      required this.icon,
+      required this.lable});
+  final Color color;
+  final IconData icon;
+  final String lable;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            lable,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.bold, color: color),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.description});
+
+  final String description;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class FavWget extends StatefulWidget {
+  const FavWget({super.key});
+
+  @override
+  State<FavWget> createState() => _FavState();
+}
+
+class _FavState extends State<FavWget> {
+  bool _isFav = true;
+  int _FavCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.center,
+            icon: (_isFav
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[300],
+            onPressed: _toggleFav,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_FavCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFav() {
+    setState(() {
+      if (_isFav) {
+        _FavCount -= 1;
+        _isFav = false;
+      } else {
+        _FavCount += 1;
+        _isFav = true;
+      }
+    });
   }
 }
