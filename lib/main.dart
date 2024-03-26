a24faad7c98e556c93a8f748a13f31c04b4e10b7
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2024-03-26 19:43:04 +0530

    show snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,52 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Grid List';
+    const String title = "Button Tap";
 
     return MaterialApp(
       title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: List.generate(50, (index) {
-            return Center(
-              child: Text(
-                'Number $index',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            );
-          }),
-        ),
+      home: MyHomePg(title: title),
+    );
+  }
+}
+
+class MyHomePg extends StatelessWidget {
+  final String title;
+  const MyHomePg({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: MyBtn(),
+    );
+  }
+}
+
+class MyBtn extends StatelessWidget {
+  const MyBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        const snack = SnackBar(content: Text("I was tapped"));
+
+        ScaffoldMessenger.of(context).showSnackBar(snack);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: Colors.blue[300], borderRadius: BorderRadius.circular(8)),
+        child: const Text('Tap ME'),
       ),
     );
   }
