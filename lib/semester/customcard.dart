cd156e7f089eba7d1221c860c557c743d3e5ab54
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2024-04-03 21:18:41 +0530

    added plugin

---
+++ lib/semester/customcard.dart
@@ -0,0 +1,92 @@
+import "package:flutter/material.dart";
+import "package:url_launcher/url_launcher.dart";
+
+class CustomCard extends StatelessWidget {
+  const CustomCard(
+      {super.key,
+      required this.title,
+      required this.imgUrl,
+      required this.fileUrl});
+
+  final String title;
+  final String imgUrl;
+  final List<String> fileUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      radius: double.infinity,
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Available Assignments'),
+                content: setupAlertDialoadContainer(),
+              );
+            });
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        color: Colors.white70,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Image.asset(imgUrl),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 26,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text('${fileUrl.length}'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Icon(Icons.arrow_circle_right_outlined),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget setupAlertDialoadContainer() {
+    return SizedBox(
+      height: 300.0, // Change as per your requirement
+      width: 300.0, // Change as per your requirement
+      child: ListView.builder(
+        itemCount: fileUrl.length,
+        itemBuilder: (context, index) {
+          final url = fileUrl[index];
+          return ListTile(
+            leading: Icon(Icons.file_download_outlined),
+            title: Text("Assignment ${index + 1}"),
+            onTap: () => _launchUrl(url),
+          );
+        },
+      ),
+    );
+  }
+}
+
+_launchUrl(String url) async {
+  // Uri.encodeComponent(url);
+  final Uri uri = Uri.parse(url);
+  if (!await launchUrl(uri)) {
+    throw Exception('Could not launch');
+  }
+}
--- lib/semester/sem6.dart
+++ lib/semester/sem6.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:hello_world/semester/customcard.dart";
 
 class CardDisplay extends StatelessWidget {
   const CardDisplay({super.key});
@@ -24,79 +25,37 @@ class CardDisplay extends StatelessWidget {
                     margin: EdgeInsets.only(bottom: 10),
                     width: double.infinity,
                     child: const CustomCard(
-                        title: "Python Programming", imgUrl: "images/lake.jpg"),
+                      title: "Python Programming",
+                      imgUrl: "images/lake.jpg",
+                      fileUrl: [
+                        'https://drive.proton.me/urls/X9TVGRJ19M#usRkZCUlQP82',
+                        'https://drive.proton.me/urls/E1DQ3QW5EM#OvFDymqW22a9'
+                      ],
+                    ),
                   ),
                   Container(
                     margin: EdgeInsets.only(bottom: 10),
                     width: double.infinity,
                     child: const CustomCard(
-                        title: "Artificial Intelligence",
-                        imgUrl: "images/lake.jpg"),
+                      title: "Artificial Intelligence",
+                      imgUrl: "images/lake.jpg",
+                      fileUrl: ['https://proton.me'],
+                    ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    width: double.infinity,
-                    child: CustomCard(title: "PHP", imgUrl: "images/lake.jpg"),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    width: double.infinity,
-                    child: CustomCard(title: "FGK", imgUrl: "images/lake.jpg"),
-                  ),
+                  // Container(
+                  //   margin: EdgeInsets.only(bottom: 10),
+                  //   width: double.infinity,
+                  //   child: CustomCard(title: "PHP", imgUrl: "images/lake.jpg"),
+                  // ),
+                  // Container(
+                  //   margin: EdgeInsets.only(bottom: 10),
+                  //   width: double.infinity,
+                  //   child: CustomCard(title: "FGK", imgUrl: "images/lake.jpg"),
+                  // ),
                 ],
               ),
             ),
           ),
         ));
-  }
-}
-
-class CustomCard extends StatelessWidget {
-  const CustomCard({super.key, required this.title, required this.imgUrl});
-
-  final String title;
-  final String imgUrl;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      radius: double.infinity,
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Tap'),
-        ));
-      },
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        color: Colors.white10,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              child: Image.asset(imgUrl),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 26,
-                    ),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(right: 8),
-                  child: Icon(Icons.arrow_circle_right_outlined),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
   }
 }
